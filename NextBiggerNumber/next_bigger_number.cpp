578bf4cc1da9425a68cfc0cd355191cf0ff363ac
Sam Tsai <sam@i3d.io>
2017-02-11 17:09:32 +0800

    Add FindNextBiggerNumber() and always return -1 that failed the test.

--- NextBiggerNumber/next_bigger_number.cpp
+++ NextBiggerNumber/next_bigger_number.cpp
@@ -1,5 +1,11 @@
 #include "../catch.hpp"
 
-TEST_CASE("Find next bigger number with the same digits", "[2-digits]")
+int FindNextBiggerNumber(int number)
 {
+	return -1;
+}
+
+TEST_CASE("Find next bigger number with the same digits", "[2-digits: 12]")
+{
+	REQUIRE(FindNextBiggerNumber(12) == 21);
 }
