0ce55b0176e028f63dbc99968105e9fba414515f
Sam Tsai <sam@i3d.io>
2017-02-13 00:21:34 +0800

    Modify FindNextBiggerNumber() to handle 2-digit number and pass the test.

--- NextBiggerNumber/next_bigger_number.cpp
+++ NextBiggerNumber/next_bigger_number.cpp
@@ -2,7 +2,11 @@
 
 int FindNextBiggerNumber(int number)
 {
-	return -1;
+	const auto ones = number % 10;
+	const auto tens = number / 10 % 10;
+	return ones * 10 + tens;
+}
+
 }
 
 TEST_CASE("Find next bigger number with the same digits", "[2-digits: 12]")
